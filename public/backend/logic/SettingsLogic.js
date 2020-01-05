2510cb7b50737b2c9f41e6b48861d5d155caed90
neonux1987 <neonux1987@gmail.com>
2020-01-05 16:17:26 +0200

    asdasd

--- public/backend/logic/SettingsLogic.js
+++ public/backend/logic/SettingsLogic.js
@@ -2,14 +2,25 @@ const IOLogic = require("../logic/IOLogic");
 const os = require('os');
 const platform = os.platform();
 const homedir = os.homedir();
+const path = require('path');
 
-const CONFIG_LOCATION = platform === "linux" ? homedir + "/Dropbox/ndts/config/config.json" : "C:/Users/ag1987/Dropbox/ndts/config/config.json";
-const CONFIG_BACKUPS_NAMES = platform === "linux" ? homedir + "/Dropbox/ndts/config/backupsNames.json" : "C:/Users/ag1987/Dropbox/ndts/config/backupsNames.json";
+const CONFIG_LOCATION = platform === "linux" ? homedir + "/Dropbox/ndts/config/config.json" : `${homedir}\\AppData\\Roaming\\ndts\\config\\config.json`;
+const CONFIG_BACKUPS_NAMES = platform === "linux" ? homedir + "/Dropbox/ndts/config/backupsNames.json" : `${homedir}\\AppData\\Roaming\\ndts\\config\\backupsNames.json`;
+const dbPath = platform === "linux" ? path.join(homedir, "Dropbox/ndts/db/ndts-frms-db.sqlite") : `${homedir}\\AppData\\Roaming\\ndts\\db\\ndts-frms-db.sqlite`;
 
 class SettingsLogic {
 
   constructor() {
     this.iOLogic = new IOLogic();
+
+    const settingsPromise = this.iOLogic.readFile(CONFIG_LOCATION);
+
+    settingsPromise.then((settings) => {
+      const parsedSettings = JSON.parse(settings);
+      parsedSettings.general.db_path = dbPath;
+      this.iOLogic.writeFile(CONFIG_LOCATION, JSON.stringify(parsedSettings, null, 2));
+    })
+
   }
 
   getSettings() {
