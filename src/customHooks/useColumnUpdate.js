5c4699abc9009280188f257dc9e0f4066f98f430
neonux1987 <neonux1987@gmail.com>
2021-03-30 04:42:38 +0300

    fixed on row delete in any table, the data stays visually but the row deleted

--- src/customHooks/useColumnUpdate.js
+++ src/customHooks/useColumnUpdate.js
@@ -1,7 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useColumnUpdate = (value) => {
   const [newValue, setNewValue] = useState(value);
+
+  // have to use useEffect since the useState
+  // runs only once to init the data and when the
+  // value updates the local state needs to be
+  // updated as well because the input value is
+  // not connected directly to the outside state
+  useEffect(() => {
+    setNewValue(() => value);
+  }, [value]);
 
   const onChange = (event) => {
     const target = event.currentTarget;
