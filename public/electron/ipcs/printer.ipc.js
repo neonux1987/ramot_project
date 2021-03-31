6111973463c161b3559389c9b2d0d7a58d682eb3
neonux1987 <neonux1987@gmail.com>
2021-03-31 17:28:09 +0300

    trying to add printing abillity

--- public/electron/ipcs/printer.ipc.js
+++ public/electron/ipcs/printer.ipc.js
@@ -1,15 +1,32 @@
-const { ipcMain, webContents } = require('electron');
-
 
 const printerIpc = () => {
+  const { ipcMain, webContents } = require('electron');
+
+  const path = require('path');
+  const fs = require('fs');
+  const SystemPaths = require("../../backend/system/SystemPaths");
 
   ipcMain.on('save-to-pdf', (event, content) => {
     const contents = webContents.getFocusedWebContents();
 
     contents.printToPDF({
-      silent: false,
-      deviceName: 'My-Printer',
-    }).catch(error => console.log(error));
+      marginsType: 0,
+      pageSize: 'A4',
+      printBackground: true,
+      printSelectionOnly: false,
+      landscape: false
+    }).then(data => {
+      console.log("data");
+
+      fs.writeFile(path.join(SystemPaths.paths.user_main_folder, "test.pdf"), data, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('PDF Generated Successfully');
+        }
+      });
+    })
+      .catch(error => console.log(error));
     event.sender.send("pdf-saved", { data: "location of the pdf file" });
   });
 
