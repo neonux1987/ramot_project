30b7943601df0f32657f0f213bd0886fc45d0a99
Andrew <neonux1987@gmail.com>
2022-05-12 06:33:46 +0300

    fixed style of toggle button

--- src/layout/ToggleButton/ToggleButton.jsx
+++ src/layout/ToggleButton/ToggleButton.jsx
@@ -10,14 +10,15 @@ const useStyles = makeStyles(theme => ({
   wrapper: {
     position: "absolute",
     top: 0,
-    left: 0,
-    zIndex: 991
+    right: 0,
+    zIndex: 991,
+    flip: false
   },
   button: {
     borderRadius: "3px",
     outline: "none",
     cursor: "pointer",
-    marginRight: "0px",
+    marginLeft: "0px",
     minWidth: "unset",
     color: "#000000",
     '-webkit-app-region': "no-drag",
@@ -27,106 +28,56 @@ const useStyles = makeStyles(theme => ({
     position: "absolute",
     width: "60px",
     height: "50px",
+    flip: false,
     '&:hover': {
       background: "none"
     },
     '&:before': {
       borderTop: "50px solid transparent",
-      borderLeft: "60px solid rgb(42 127 214)",
+      borderRight: `60px solid ${theme.palette.primary.main}`,
       transform: "rotateX(180deg)",
       content: '""',
       zIndex: 887,
       position: "absolute",
+      flip: false
     }
   },
   iconWrapper: {
     transform: `rotate(314deg)`,
     zIndex: "999",
-    position: "relative"
+    position: "absolute",
+    top: "4px",
+    right: "10px",
+    flip: false
   },
   iconStyle: {
     width: "24px",
     height: "24px",
     color: "#f1f1f1",
-    left: "-16px",
+    right: 0,
     top: "-8px",
+    position: "absolute",
+    flip: false
   },
   nonClickable: {
     borderTop: "50px solid transparent",
-    borderLeft: "60px solid transparent",
+    borderRight: "60px solid transparent",
     content: "",
     zIndex: "999",
     position: "absolute",
     transform: "skew(50deg)",
     top: "0px",
     left: "30px",
+    flip: false
   }
 }));
-
-const _wrapper = css`
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: 991;
-`;
-
-const _button = css`
-  border-radius: 3px;
-  outline: none;
-  cursor: pointer;
-  margin-right: 0px;
-  min-width: unset;
-  color: #000000;
-  -webkit-app-region: no-drag;
-  -webkit-user-select: none;
-  padding: 0;
-  z-index: 888;
-  position: absolute;
-  width: 60px;
-  height: 50px;
-
-  :hover{
-    background: none;
-  }
-
-  :before{
-    border-top: 50px solid transparent;
-    border-right: 60px solid rgb(42 127 214);
-    transform: rotateX(180deg);
-    content: "";
-    z-index: 887;
-    position: absolute;
-  }
-`;
-
-const iconStyle = css`
-  width: 24px;
-  height: 24px;
-  color: #f1f1f1;
-  z-index: 999;
-  right: -8px;
-  position: relative;
-  top: -4px;
-  transform: rotate(338deg);
-`;
-
-const nonClickable = css`
-  border-top: 50px solid transparent;
-  border-right: 60px solid transparent;
-  content: "";
-  z-index: 999;
-  position: absolute;
-  transform: skew(50deg);
-  top: 0px;
-  right: 30px;
-`;
 
 const ToggleButton = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const showSidebar = useSelector(store => store.toggleSidebar.showSidebar);
 
-  const icon = showSidebar ? <ArrowForwardIos className={classes.iconStyle} /> : <ArrowBackIos className={classes.iconStyle} style={{ right: "-11px" }} />;
+  const icon = showSidebar ? <ArrowForwardIos className={classes.iconStyle} /> : <ArrowBackIos className={classes.iconStyle} style={{ right: "-3px" }} />;
 
   const onClick = () => dispatch(toggleSidebar());
 
