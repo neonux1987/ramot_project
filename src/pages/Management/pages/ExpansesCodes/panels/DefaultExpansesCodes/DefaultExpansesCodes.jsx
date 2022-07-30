6a11e58003451e380acee32c570b6bf1c6be922c
neonux1987 <neonux1987@gmail.com>
2022-07-30 09:54:27 +0300

    added description header to default expanses codes transfer page

--- src/pages/Management/pages/ExpansesCodes/panels/DefaultExpansesCodes/DefaultExpansesCodes.jsx
+++ src/pages/Management/pages/ExpansesCodes/panels/DefaultExpansesCodes/DefaultExpansesCodes.jsx
@@ -3,11 +3,18 @@ import Box from "@material-ui/core/Box";
 import SectionWithHeader from "../../../../../../components/Section/SectionWithHeader";
 import SectionControlsContainer from "../../../../../../components/table/TableControls/SectionControlsContainer";
 import DefaultExpnansesCodesTransferList from "./DefaultExpnansesCodesTransferList";
+import { Typography } from "@material-ui/core";
 
 const DefaultExpansesCodes = () => {
   return (
     <SectionWithHeader>
       <SectionControlsContainer edit={false} addNew={false} print={false} />
+
+      <Box display="flex" justifyContent="center">
+        <Typography variant="h5" fontWeight="600">
+          רשימת קודי הברירת מחדל משמשים ליצירת דוחות ריקים
+        </Typography>
+      </Box>
 
       <Box minHeight={"600px"} padding="20px">
         <DefaultExpnansesCodesTransferList />
