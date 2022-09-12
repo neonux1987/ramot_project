07d413a9abbdd07f0dc7da8a3d911c944d5aef0f
neonux1987 <neonux1987@gmail.com>
2022-09-12 16:38:56 +0300

    chore: hide print window and destroy it on print

--- electron/ipcs/printer.ipc.js
+++ electron/ipcs/printer.ipc.js
@@ -4,7 +4,7 @@ const printerIpc = () => {
   ipcMain.on("print-pdf", async (event, options = {}, blobUrl = null) => {
     let win = new BrowserWindow({
       title: "Print Preview",
-      show: true,
+      show: false,
       autoHideMenuBar: true,
       webPreferences: {
         nodeIntegration: true,
@@ -59,7 +59,7 @@ const printerIpc = () => {
         return;
       }
 
-      //win.destroy();
+      win.destroy();
 
       const { pageCount, output } = await addPageNumbers(data);
 
