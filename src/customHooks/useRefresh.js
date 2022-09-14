8e2449a01676a7fb62bb855634fb940a581dc7a8
neonux1987 <neonux1987@gmail.com>
2022-09-14 19:37:53 +0300

    fix: add useIsMounted to useRefresh to prevent memory leaks

--- src/customHooks/useRefresh.js
+++ src/customHooks/useRefresh.js
@@ -1,6 +1,6 @@
 import { ipcRenderer } from "electron";
 import { useEffect, useState } from "react";
-
+import useIsMounted from "./useIsMounted";
 /* 
   this hook listens to an event refresh and will execute a callback
   when the refresh event fired. usually on some kind of state change
@@ -8,10 +8,11 @@ import { useEffect, useState } from "react";
 */
 const useRefresh = () => {
   const [refresh, setRefresh] = useState(false);
+  const isMounted = useIsMounted;
 
   useEffect(() => {
     const listener = () => {
-      setRefresh(true);
+      if (isMounted()) setRefresh(true);
     };
     // remove previous listener if exist
     // before registering a new one
@@ -22,7 +23,7 @@ const useRefresh = () => {
     return () => {
       ipcRenderer.removeAllListeners("refresh");
     };
-  }, []);
+  }, [isMounted]);
 
   return [refresh, setRefresh];
 };
