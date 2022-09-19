40295b16f941a9ae845597b01f9f90a9f1bdc22b
Andrew <neonux1987@gmail.com>
2022-09-20 02:01:32 +0300

    fix: MoreBuildingMenu showing in home page

--- src/redux/reducers/routesReducer.js
+++ src/redux/reducers/routesReducer.js
@@ -1,32 +1,34 @@
-import { TYPES } from '../actions/routesActions';
+import { TYPES } from "../actions/routesActions";
 
 const initState = {
   active: {
     pathname: "/דף-הבית",
     state: {
       page: "דף הבית",
-      buildingName: "",
-      buildingId: ""
+      buildingName: "דף הבית",
+      buildingId: "home"
     },
     expanded: {}
   }
-}
+};
 
 const routesReducer = (state = initState, action) => {
   switch (action.type) {
-    case TYPES.ROUTES_UPDATE: return {
-      ...state,
-      active: {
-        ...state.active,
-        ...action.active,
-        expanded: {
-          ...state.active.expanded,
-          ...action.active.expanded
+    case TYPES.ROUTES_UPDATE:
+      return {
+        ...state,
+        active: {
+          ...state.active,
+          ...action.active,
+          expanded: {
+            ...state.active.expanded,
+            ...action.active.expanded
+          }
         }
-      }
-    }
-    default: return state;
+      };
+    default:
+      return state;
   }
-}
+};
 
 export default routesReducer;
