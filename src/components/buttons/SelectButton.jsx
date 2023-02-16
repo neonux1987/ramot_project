e6f0b247f614349606301012e309774f08b63aa5
Andrew <neonux1987@gmail.com>
2023-02-16 05:30:43 +0200

    refactor: replace select button instead of text icon

--- src/components/buttons/SelectButton.jsx
+++ src/components/buttons/SelectButton.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import { css } from "emotion";
-import MenuItem from "@material-ui/core/MenuItem";
+import React from "react";
+import SettingsIcon from "../Icons/SettingsIcon";
 
 const _select = css`
   text-align: center;
   background-color: #ffffff;
   color: #000000;
   border-radius: 6px;
-  border: 1px solid #dddddd;
-  background-color: #fafafa;
 
   :before {
     border-bottom: none;
+    display: none;
   }
 
   :after {
@@ -22,10 +22,14 @@ const _select = css`
       border: none;
     }
   }
+
+  :hover {
+    background-color: #fafafa;
+  }
 `;
 
 const _classesSelect = css`
-  padding: 8px 10px 6px 24px;
+  padding: 8px 10px 6px 10px !important;
   font-weight: 500;
 
   :focus {
@@ -33,16 +37,22 @@ const _classesSelect = css`
   }
 `;
 
+const _classesSelectIcon = css`
+  display: none;
+`;
+
 const SelectButton = ({ onClick, label = "ללא שם" }) => {
   return (
     <Select
       className={_select}
       value={label}
-      classes={{ select: _classesSelect }}
+      classes={{ select: _classesSelect, icon: _classesSelectIcon }}
       onClick={onClick}
       open={false}
     >
-      <MenuItem value={label}>פעולות נוספות</MenuItem>
+      <MenuItem value={label}>
+        <SettingsIcon />
+      </MenuItem>
     </Select>
   );
 };
